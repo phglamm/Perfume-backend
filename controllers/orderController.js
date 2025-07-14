6ef483535253bd9b5d008907c698ef9eee253e6f
phonglamm <lamphong1507@gmail.com>
2025-07-14 08:11:44 +0700

    Update get Order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -28,3 +28,30 @@ exports.getOrdersForUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getOrderById = async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+    const order = await Order.findById(orderId)
+      .populate({
+        path: "orderItems",
+        populate: {
+          path: "perfume",
+          select: "perfumeName uri price concentration ",
+        },
+        select: "quantity totalPrice",
+      })
+      .populate("user", "username email fullName phone")
+      .select("-__v  -updatedAt");
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.status(200).json({
+      status: 200,
+      message: "Order retrieved successfully",
+      order,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -4,5 +4,5 @@ const orderController = require("../controllers/orderController");
 const { authenticateUser } = require("../middleware/authenticatedMiddleware");
 
 orderRouter.get("/user", authenticateUser, orderController.getOrdersForUser);
-
+orderRouter.get("/:orderId", authenticateUser, orderController.getOrderById);
 module.exports = orderRouter;
